2bebc57ba0e33cc71f2e3206c9f9a6206aca7402
abdel-ke <kemmal1337@gmail.com>
2024-12-31 12:08:20 +0100

    ex02

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+def all_thing_is_obj(object: any) -> int:
+    t = type(object)
+    if t is list:
+        print('List :', t)
+    elif t is tuple:
+        print('Tuple :', t)
+    elif t is set:
+        print('Set :', t)
+    elif t is dict:
+        print('Dict :', t)
+    elif t is str:
+        print(object,'is in the kitchen :', t)
+    else:
+        print('Type not found')
+    return 42
